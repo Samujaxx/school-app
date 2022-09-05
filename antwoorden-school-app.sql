40ba3f2bcdf7aece266ce4c9c746c81999cf9617
samujaxx <samuraijax2003@gmail.com>
2022-09-05 09:23:05 +0200

    v1.2

--- antwoorden-school-app.sql
+++ antwoorden-school-app.sql
@@ -7,3 +7,7 @@ WHERE plaatsnaam <> 'Haarlem' AND beurs > 90
 ORDER BY plaatsnaam;
 
 --oprdracht 2--
+SELECT naam AS voornaam, studie.studie, plaats.plaatsnaam AS woonplaats FROM student
+JOIN studie on studie.code = student.studie
+JOIN plaats ON plaats.code = student.woonplaats
+WHERE studie.plaats <> "Den Haag";
