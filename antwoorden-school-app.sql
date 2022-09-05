650cab86d794b5bd45b1465fb043cfb223a1e9a2
samujaxx <samuraijax2003@gmail.com>
2022-09-05 09:08:44 +0200

    v1.11

--- antwoorden-school-app.sql
+++ antwoorden-school-app.sql
@@ -3,6 +3,6 @@
 --Opdracht 1--
 SELECT naam, woonplaats FROM student
 JOIN plaats ON plaats.code = student.woonplaats
-WHERE woonplaats <> 'Haarlem' AND beurs > 90;
+WHERE plaatsnaam <> 'Haarlem' AND beurs > 90;
 
 --oprdracht 2--
